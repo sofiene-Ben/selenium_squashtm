addb00d8799956e5b1903f2980aa7c439e16a755
Sofiene Benamar <Sofiene.benamar@hotmail.com>
2026-03-02 18:27:24 +0100

    add headless

--- test_main.py
+++ test_main.py
@@ -8,9 +8,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture
 def driver():
     options = Options()
-    # options.add_argument("--headless")  # obligatoire en CI
-    # options.add_argument("--no-sandbox")
-    # options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--headless")  # obligatoire en CI
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
